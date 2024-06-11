d530e632a6419196751bae4d4188e34f1ace4551
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-12 00:01:11 +0300

    arrays

---
+++ numpy_arrays.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#result = np.array([1,2,3,4,5,6])
+
+#result = np.arange(1,7)
+#result = np.arange(10,50,2)
+#result = np.zeros(10)
+#result = np.ones(10)
+#result = np.linspace(0,100,5)
+#result = np.random.randint(0,10)
+#result = np.random.randint(0,50,5)
+#result = np.random.rand(3)
+#result = np.random.randn(3)
+
+print(result)
